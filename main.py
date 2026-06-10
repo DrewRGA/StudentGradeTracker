4803c407e644c62b690b55f5a5080c9f9a07d40b
Andrew Garcia <drewrga@gmail.com>
2026-06-10 15:14:26 -0700

    Add student sorting and grade statistics

--- main.py
+++ main.py
@@ -7,13 +7,11 @@ class Student:
         self.age = age
         self.grade = grade
 
-bill = Student("bill", 15, 77)
-class_roster.append(bill)
-sarah = Student("sara", 15, 71)
-class_roster.append(sarah)
-bob = Student("bob", 14, 79)
-class_roster.append(bob)
+    def display(self):
+        print(f"Name: {self.name}, Age: {self.age}, Grade: {self.grade}.")
 
+    def update_grade(self, new_grade):
+        self.grade = new_grade
 
 def add_student():
     name = str(input("Enter their name: "))
@@ -33,11 +31,15 @@ def view_student():
         print("There are no students in the class.")
     else:
         for student in class_roster:
-            print(f"Name: {student.name}, Age: {student.age}, Grade: {student.grade}.")
+            student.display()
 
     print("\n")
 
 def search_students():
+    if not class_roster:
+        print("There are no students in the class.\n")
+        return
+
     user_search = str(input("Please type the name of the student you would like to search: \n")).lower()
 
     for student in class_roster:
@@ -49,8 +51,11 @@ def search_students():
     return
 
 def grade_average():
+    if not class_roster:
+        print("There are no students in the class. Average cannot be computed.\n")
+        return
+
     average = 0.0
-    i = 0
 
     for student in class_roster:
         average = average + student.grade
@@ -60,6 +65,10 @@ def grade_average():
     print(f"The class has a grade average of: {average}\n")
 
 def remove_student():
+    if not class_roster:
+        print("There is nobody to remove from the class.\n")
+        return
+
     user_name = str(input("Who would you like to remove? ")).lower()
 
     for student in class_roster:
@@ -71,12 +80,16 @@ def remove_student():
     print(f"{user_name}, could not be removed as they are not in the class roster. \n")
 
 def update_grade():
+    if not class_roster:
+        print("There are no students in the class. No grades can be updated.\n")
+        return
+
     user_name = (input("Which Student needs their grade updated? ")).lower()
     user_grade = int(input("What is their new grade? "))
 
     for student in class_roster:
         if user_name == student.name:
-            student.grade = user_grade
+            student.update_grade(user_grade)
             print(f"{student.name}'s grade has been updated.\n")
             return
 
@@ -93,15 +106,73 @@ def load_students():
         for line in file:
             line = line.strip()
             line = line.split(",")  # Creates into a list of strings
-            load = Student(line[0],line[1],line[2])
+            load = Student(str(line[0]),int(line[1]),int(line[2]))
             class_roster.append(load)
+
+def sort_alpha():
+    if not class_roster:
+        print("There are no students in the class to sort.\n")
+        return
+
+    class_roster.sort(key=lambda student: student.name)
+    print("The class is now sorted by name.\n")
+
+def sort_grade():
+    if not class_roster:
+        print("There are no students in the class to sort.\n")
+        return
+
+    class_roster.sort(key=lambda student: student.grade)
+    print("The class is now sorted by grade.\n")
+
+def highest_grade():
+    if not class_roster:
+        print("There are no students in the class.\n")
+        return
+
+    temp_grade = 0
+    temp_name = ""
+
+    for student in class_roster:
+        if student.grade > temp_grade:
+            temp_grade = student.grade
+            temp_name = student.name
+    print(f"{temp_name} has the highest grade of {temp_grade}%\n")
+
+def lowest_grade():
+    if not class_roster:
+        print("There are no students in the class to sort.\n")
+        return
+
+    temp_grade = 100
+    temp_name = ""
+
+    for student in class_roster:
+        if student.grade < temp_grade:
+            temp_grade = student.grade
+            temp_name = student.name
+    print(f"{temp_name} has the lowest grade of {temp_grade}%\n")
 
 load_students()
 while menu is True:
-    print("1. Add Student", "2. View Students", "3. Search Student", "4. Calculate Average Grade", "5. Update Student Grade", "6. Remove Student","7. Exit", sep="\n")
-    print("\n")
-    selection = int(input("Please enter 1 - 7 to pick an option: "))
-    print("\n")
+    print(
+        "1. Add Student",
+        "2. View Students",
+        "3. Search Student",
+        "4. Calculate Average Grade",
+        "5. Update Student Grade",
+        "6. Remove Student",
+        "7. Sort Students Alphabetically",
+        "8. Sort Students by Grade",
+        "9. View Highest Grade",
+        "10. View Lowest Grade",
+        "11. Exit",
+        sep="\n"
+    )
+    print()
+
+    selection = int(input("Please enter 1 - 11 to pick an option: "))
+    print()
 
     if selection == 1:
         add_student()
@@ -120,6 +191,18 @@ while menu is True:
 
     elif selection == 6:
         remove_student()
+
+    elif selection == 7:
+        sort_alpha()
+
+    elif selection == 8:
+        sort_grade()
+
+    elif selection == 9:
+        highest_grade()
+
+    elif selection == 10:
+        lowest_grade()
 
     else:
         save_students()
