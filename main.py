182c31a25831fa74e4fa13d69758be11cf8dc08d
Andrew Garcia <drewrga@gmail.com>
2026-06-11 20:50:30 -0700

    Move Student class to seperate module

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
+from student import Student
+
 menu = True
 class_roster = []
-
-class Student:
-    def __init__(self, name, age, grade):
-        self.name = name
-        self.age = age
-        self.grade = grade
-
-    def display(self):
-        print(f"Name: {self.name}, Age: {self.age}, Grade: {self.grade}.")
-
-    def update_grade(self, new_grade):
-        self.grade = new_grade
 
 def add_student():
     name = str(input("Enter their name: "))
---
+++ student.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+
+    def display(self):
+        print(f"Name: {self.name}, Age: {self.age}, Grade: {self.grade}.")
+
+    def update_grade(self, new_grade):
+        self.grade = new_grade
